7b86a204f5268bea4c570127c240f54d869cc8e6
LastCeri <125711498+LastCeri@users.noreply.github.com>
2023-08-16 18:46:30 +0300

    Api Update

--- src/api/api.js
+++ src/api/api.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
-app.get('/Api', (req, res) => {
+app.get('/', (req, res) => {
     console.log("GET isteği alındı.");
     res.json({ message: "Merhaba, bu bir API yanıtıdır!" });
 });
 
-app.post('/Api', (req, res) => {
+app.post('/', (req, res) => {
     console.log("POST isteği alındı. Gönderilen veri:", req.body);
     res.json({ message: "Veri başarıyla alındı!" });
 });
