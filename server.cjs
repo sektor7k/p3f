86905d074ed0d76520d6ff72ffb0818096c65dce
LastCeri <125711498+LastCeri@users.noreply.github.com>
2023-08-16 18:32:59 +0300

    Api Update

--- server.cjs
+++ server.cjs
@@ -1,24 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const serverless = require('serverless-http');
 
 const app = express();
-const PORT = 3001;
 
 app.use(cors());
-app.use(express.json()); // Bu middleware'i ekleyerek gelen JSON verisini okuyabiliriz.
+app.use(express.json()); 
 
 app.get('/Api', (req, res) => {
+    console.log("GET isteği alındı.");
     res.json({ message: "Merhaba, bu bir API yanıtıdır!" });
 });
 
-// Yeni bir POST endpoint'i oluşturalım.
 app.post('/Api', (req, res) => {
-    console.log(req.body); // Burada gelen JSON veriyi konsola yazdırıyoruz.
-
-    // İsteğe yanıt olarak bir mesaj gönderelim.
+    console.log("POST isteği alındı. Gönderilen veri:", req.body);
     res.json({ message: "Veri başarıyla alındı!" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = serverless(app);
